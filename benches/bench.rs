085fe7815a5b95339b37581555ebe002e556a2c4
svorbrugg <sebastian.vorbrugg@student.uni-tuebingen.de>
2023-10-06 16:48:40 +0200

    Removed all bench.

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,12 +1,12 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use related_intervals::{make_nested2, make_nested22, sort_vector};
+use related_intervals::{make_nested, sort_vector};
 
 use std::cmp::Ordering;
 
 
 /// Create a vector of tuples
 /// These are in order
-fn creat_hashset() -> Vec<(u32, u32)>{
+fn create_vector_linear() -> Vec<(u32, u32)>{
     let mut vector = Vec::new();
     //vector.push((0, 1000000));
     for value in 1..10000{
@@ -19,64 +19,52 @@ fn creat_hashset() -> Vec<(u32, u32)>{
 
 /// Create a vector of tuples
 /// These are in order
-fn creat_hashset2() -> Vec<[u32; 2]>{
+fn create_vector_jumps() -> Vec<[u32; 3]>{
     let mut vector = Vec::new();
     //vector.push((0, 1000000));
-    for value in 1..10000{
-        if value % 500 == 0{
-            vector.push([value +1, value +2000]);
+    for value in 1..100000{
+        if value % 5000 == 0{
+
+            vector.push([value +1, value +200000, 1]);
         } else if value % 200 == 0{
-            vector.push([value +1, value +500]);
+            vector.push([value +1, value +50, 1]);
 
         }
 
         else {
-            vector.push([value +1, value +2]);
+            vector.push([value +1, value +2, 1]);
 
         }
     }
-    sort_vector2(&mut vector);
+    sort_vector(&mut vector);
     return vector
 
 }
 
-/// Interval vector sorting
-pub fn sort_vector2(intervals: &mut Vec<[u32; 2]>){
-    intervals.sort_by(|a, b| (a[0].cmp(&b[0]).then(b[1].cmp(&a[1]))));
-
-}
 
 
 
 
 
 
-/// Run the new function
-fn testt(){
-    let mut intervals = creat_hashset2();
+/// Testing the stuff
+fn test_nested(intervals: &Vec<[u32; 3]>){
+
     //sort_vector(&mut intervals);
-    let mut network = make_nested2(&intervals);
+    let mut network = make_nested(&intervals);
 }
-
-//
-//
-// /// Run the new function
-// fn testt2(){
-//     let mut intervals = creat_hashset2();
-//     //sort_vector(&mut intervals);
-//     let mut network = make_nested22(&intervals);
-// }
 
 
 
 
 fn criterion_benchmark(c: &mut Criterion) {
+    let mut intervals = create_vector_jumps();
 
 
 
 
     //c.bench_function("faster network", |b| b.iter(|| test()));
-    c.bench_function("faster network", |b| b.iter(|| testt()));
+    c.bench_function("faster network", |b| b.iter(|| test_nested(&intervals)));
     // c.bench_function("faster network", |b| b.iter(|| testt2()));
 
 
